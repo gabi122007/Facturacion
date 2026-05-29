73ad82e5073a0595b52c378d5a9274fb32be3a2e
gabi122007 <gabiespr07@gmail.com>
2026-05-29 15:27:00 -0600

    leccion 1: primeros pasos

---
+++ src/main/java/com/tuEmpresa/Facturacion/modelo/Cliente.java
@@ -0,0 +1,25 @@
+package com.tuEmpresa.Facturacion.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+import org.openxava.annotations.Required;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter @Setter
+public class Cliente {
+
+    @Id
+    @Column(length=6)
+    private int numero;
+
+    @Column
+    @Required
+    private String nombre;
+
+
+}
---
+++ src/main/java/com/tuEmpresa/Facturacion/modelo/Producto.java
@@ -0,0 +1,21 @@
+package com.tuEmpresa.Facturacion.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.openxava.annotations.Required;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Producto {
+    @Id
+    @Column(length=9)
+    int numero;
+
+    @Column(length=50) @Required
+    String descripcion;
+}
